2ba9e0f7c5a458b7451846534fafde613d1a4c96
Kulunchick <nikolikrasovski@gmail.com>
2024-12-27 10:19:07 +0200

    fix: add tests folder and lint config

--- api-gateway/app/utils.py
+++ api-gateway/app/utils.py
@@ -11,22 +11,22 @@ from services.user import user_pb2_grpc
 
 
 class GrpcProvider(Provider):
-    @provide(scope=Scope.APP)
+    @provide(scope=Scope.REQUEST)
     async def get_track_service(self, settings: Settings) -> AsyncIterable[track_pb2_grpc.TrackStub]:
         async with grpc.aio.insecure_channel(f'{settings.TRACK_SERVICE_GRPC_HOST}:{settings.TRACK_SERVICE_GRPC_PORT}') as channel:
             yield track_pb2_grpc.TrackStub(channel)
 
-    @provide(scope=Scope.APP)
+    @provide(scope=Scope.REQUEST)
     async def get_auth_service(self, settings: Settings) -> AsyncIterable[auth_pb2_grpc.AuthStub]:
         async with grpc.aio.insecure_channel(f'{settings.AUTH_SERVICE_GRPC_HOST}:{settings.AUTH_SERVICE_GRPC_PORT}') as channel:
             yield auth_pb2_grpc.AuthStub(channel)
 
-    @provide(scope=Scope.APP)
+    @provide(scope=Scope.REQUEST)
     async def get_playlist_service(self, settings: Settings) -> AsyncIterable[playlist_pb2_grpc.PlaylistStub]:
         async with grpc.aio.insecure_channel(f'{settings.PLAYLIST_SERVICE_GRPC_HOST}:{settings.PLAYLIST_SERVICE_GRPC_PORT}') as channel:
             yield playlist_pb2_grpc.PlaylistStub(channel)
 
-    @provide(scope=Scope.APP)
+    @provide(scope=Scope.REQUEST)
     async def get_user_service(self, settings: Settings) -> AsyncIterable[user_pb2_grpc.UserStub]:
         async with grpc.aio.insecure_channel(f'{settings.USER_SERVICE_GRPC_HOST}:{settings.USER_SERVICE_GRPC_PORT}') as channel:
             yield user_pb2_grpc.UserStub(channel)
